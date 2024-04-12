85f606038fb97737790148f17c375af76c3a7f09
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2024-04-12 13:05:49 -0400

    Set Up Routes in React

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import Root from "./routes/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import NavBar from "./components/utils/NavBar";
+import Footer from "./components/utils/Footer";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+	},
+	{
+		path: "/error",
+		element: <ErrorPage />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<NavBar />
+		<RouterProvider router={router} />
+		<Footer />
 	</React.StrictMode>,
 );
---
+++ frontend/src/routes/Root.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Root() {
+	return (
+		<>
+			<h1>React Router Contacts</h1>
+		</>
+	);
+}
