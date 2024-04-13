019ba1d939c5ceb3cb728d74d9a989b7222abd53
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2024-04-12 23:07:10 -0400

    Finalize (hopefully) Julian layout

--- frontend/src/components/workspaces/layouts/julian/Julian.jsx
+++ frontend/src/components/workspaces/layouts/julian/Julian.jsx
@@ -1,38 +1,8 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import JulianRow from "./JulianRow";
-import MeetingSet from "../../sets/MeetingSet";
-import ClassRoom from "../../rooms/ClassRoom";
 import styles from "./Julian.module.css";
 
 const Julian = () => {
-	const rows = 5;
-	const cols = 5;
-
-	const layoutPositions = {
-		0: { 0: ClassRoom },
-		1: { 0: ClassRoom, 2: MeetingSet, 4: MeetingSet },
-		3: { 0: ClassRoom },
-		4: { 0: ClassRoom, 2: MeetingSet, 4: MeetingSet },
-	};
-
-	return (
-		<>
-			<div>Julian</div>
-			<Container className={styles.julianContainer}>
-				{Array.from({ length: rows }, (_, rowIndex) => (
-					<JulianRow
-						key={rowIndex}
-						cells={cols}
-						renderComponent={colIndex => {
-							const ComponentToRender = layoutPositions[rowIndex]?.[colIndex];
-							return ComponentToRender ? <ComponentToRender /> : null;
-						}}
-					/>
-				))}
-			</Container>
-		</>
-	);
+	return <div className={styles.julianContainer}></div>;
 };
 
 export default Julian;
